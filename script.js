fdf4efffa3fc1b56d2b8124d3782f89d1dc981cc
Real-Kami <67558858+Real-Kami@users.noreply.github.com>
2023-05-10 20:44:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let dat = [
+    "C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G", "G#", "Ab", "A", "A#", "Bb", "B",
+    "Cm", "C#m", "Dbm", "Dm", "D#m", "Ebm", "Em", "Fm", "F#m", "Gbm", "Gm", "G#m", "Abm", "Am", "A#m", "Bbm", "Bm"
+]
+
+let cont = document.getElementById("cont");
+
+function test() {
+    let r = Math.floor( Math.random() * dat.length )
+    cont.innerText = dat[r];
+}
